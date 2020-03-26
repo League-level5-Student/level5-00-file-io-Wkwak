ccbfad11218b1c7cbaf512ce6eea5fe65d99529d
William Kwak <34603372+Wkwak@users.noreply.github.com>
2020-03-25 17:24:34 -0700

    Finished level 5 module 0 up to file encryptor

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,23 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String words = JOptionPane.showInputDialog(null, "write somethng");
+		
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/test.txt", true);
+			
+			fw.write(words);
+			fw.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,13 +1,15 @@
 package _02_File_Encrypt_Decrypt;
 
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	/*
-	 * Encryption is the process of encoding a message or information
-	 * in such a way that only authorized parties can access it and
-	 * those who are not authorized cannot.
+	 * Encryption is the process of encoding a message or information in such a way
+	 * that only authorized parties can access it and those who are not authorized
+	 * cannot.
 	 *
-	 * A simple shift cipher works by shifting the letters of a message
-	 * down based on a key. If our key is 4 then:
+	 * A simple shift cipher works by shifting the letters of a message down based
+	 * on a key. If our key is 4 then:
 	 * 
 	 * a b c d e f g h i j k l m n o p q r s t u v w x y z
 	 * 
@@ -17,7 +19,39 @@ public class FileEncryptor {
 	 * 
 	 * "Hello World" changes to "Lipps Asvph"
 	 *
-	 * Create a program that takes a message and a key from the user.
-	 * Use the key to shift each letter in the users input and save the final result to a file.
+	 * Create a program that takes a message and a key from the user. Use the key to
+	 * shift each letter in the users input and save the final result to a file.
 	 */
+
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog(null, "Write the message to be encrypted");
+		String key = JOptionPane.showInputDialog(null, "Type in the key you want to shift each letter by");
+		int actualKey = Integer.parseInt(key);
+
+		FileEncryptor fe = new FileEncryptor();
+		String encryptedInput = fe.encrypt(input, actualKey);
+
+		System.out.println(encryptedInput);
+	}
+
+	public String encrypt(String s, int key) {
+		String newS = "";
+		int newAscii;
+
+		for (int i = 0; i < s.length(); i++) {
+			int ascii = (int) s.charAt(i);
+			newAscii = ascii + key;
+			
+			if ((int)Character.toLowerCase(s.charAt(i)) + key > (int)'z') {
+				
+				newAscii = ascii + key - 26;
+				newS += (char) newAscii;
+			} else {
+				newS += (char) newAscii;
+			}
+
+		}
+
+		return newS;
+	}
 }
